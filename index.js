e4098f2bd503b352fa416cb285c59f88e01711c9
Ji Sheng <jsheng0811@gmail.com>
2020-03-31 23:39:33 +0800

    Added Postgresql to Hapi

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const env = require('env2')("env.json");
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
         host: 'localhost'
+    });
+
+    await server.register([{
+        plugin: require('hapi-pgsql'),
+        options: {
+            database_url: process.env.DATABASE_URL
+        }
+    }]);
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: async (request, h) => {
+            const time = await request.pgsql.query('SELECT NOW()')
+            return `Hello World! Time: ${time.rows[0].now}`;
+        }
     });
 
     await server.start();
